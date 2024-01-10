999cdf754ef804ecdd3ff6e200076ef4eec6cb5f
NeoMatlala <neomatlala70@gmail.com>
2024-01-10 23:05:33 +0200

    FE: Homepage

---
+++ RR.UI/pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="max-w-7xl mx-auto">
+        <h1 class="ml-56 text-right leading-relaxed text-7xl font-medium">
+            Where Recording, Mixing, Mastering, and Rehearsal <br> Unite in Perfect Harmony.
+        </h1>
+
+        <div class="w-1/6 h-28 flex justify-start gap-x-3 items-center">
+            <div class="border-yellow-400 h-full border-l-4"></div>
+            <div>
+                <h6 class="font-medium text-lg">R&R studios</h6>
+                <p class="mt-2 font-light text-slate-500">Harmonize Your <br> Sound, Elevate Your <br> Craft</p>
+            </div>
+        </div>
+
+        <div class="mt-10 2xl:mt-16">
+            <h1 class="text-3xl mb-5 font-medium">Have a look at who's been in our studio ...</h1>
+
+            <div class="flex gap-6 justify-between h-56 py-5 2xl:h-[250px] items-center">
+                <div class="bg-cover h-full w-1/4">
+                    <img src="../assets/img/artist-1.jpg" alt="" class="w-full h-full">
+                </div>
+                <div class="bg-cover h-full w-1/4">
+                    <img src="../assets/img/artist-2.jpg" alt="" class="w-full h-full">
+                </div>
+                <div class="bg-cover h-full w-1/4">
+                    <img src="../assets/img/artist-3.jpg" alt="" class="w-full h-full">
+                </div>
+                <div class="bg-cover h-full w-1/4">
+                    <img src="../assets/img/artist-4.jpg" alt="" class="w-full h-full">
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-24 2xl:my-16 flex justify-between items-center gap-10">
+            <div class="w-1/2 h-[500px]">
+                <img src="../assets/img/studio.jpg" alt="" class="w-full h-full">
+            </div>
+
+            <div class="w-1/2 ">
+                <small class="text-slate-400">We love what we do</small>
+                <h1 class="text-8xl font-medium">Services</h1>
+
+                <h6 class="text-lg mt-10 mb-2 font-medium">Reheasal space</h6>
+                <p class="text-slate-400 leading-relaxed">Perfect your performance in our state-of-the-art rehearsal spaces designed to amplify your musical potential.</p>
+
+                <div class="mt-10 flex justify-between gap-x-10 items-start">
+                    <div class="w-1/2">
+                        <h6 class="text-lg mb-2 font-medium">Mixing & Mastering</h6>
+                        <p class="text-slate-400 leading-relaxed">Refine your artistry with precision – our studio offers expert mixing and mastering services for a polished and professional sonic experience.</p>
+                    </div>
+                    <div class="w-1/2">
+                        <h6 class="text-lg mb-2 font-medium">Recording</h6>
+                        <p class="text-slate-400 leading-relaxed">Elevate your sound with our cutting-edge studio, where every note is captured and crafted to perfection.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
