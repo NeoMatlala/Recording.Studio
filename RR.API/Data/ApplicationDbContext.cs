f7c850d21d4047e14bdf6da952871a2a12baf885
NeoMatlala <neomatlala70@gmail.com>
2024-01-11 12:40:46 +0200

    asp.netcore Identity setup & configuration | DB Context | Server & client side user registration | CORS configuration

---
+++ RR.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RR.API.Models;
+
+namespace RR.API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ RR.API/ViewModels/UserManagerResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace RR.API.ViewModels
+{
+    // a class to wrap the response from the api calls
+    public class UserManagerResponse
+    {
+        public string Message { get; set; }
+
+        public bool IsSuccess { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
